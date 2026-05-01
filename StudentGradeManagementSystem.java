06f173e30c0d438a52203356f04ee30fcc5b77d6
masonunderw00d <masonunderwood77@gmail.com>
2026-05-01 12:24:16 -0500

    Completed the add student and grade feature. Removed static from methods and data fields

--- StudentGradeManagementSystem.java
+++ StudentGradeManagementSystem.java
@@ -2,13 +2,19 @@ import java.util.Scanner;
 
 public class StudentGradeManagementSystem {
 
-    Scanner input = new Scanner(System.in)
+    private Scanner input = new Scanner(System.in);
 
-    String[] names = new String[50];
-    int[] grades = new int[50];
-    int count = 0
+    private String[] names = new String[50];
+    private int[] grades = new int[50];
+    private int count = 0;
     public static void main(String[] args) {
+        StudentGradeManagementSystem system = new StudentGradeManagementSystem();
+        system.menuLoop();
+    }
+
+    public void menuLoop() {
         int choice = 0;
+    
 
         while (choice != 6) {
             showMenu();
@@ -30,15 +36,15 @@ public class StudentGradeManagementSystem {
             } else if (choice == 6) {
                 System.out.println("Exiting the program...");
             } else {
-                System.out.println("Invalid choice. Please select an option between 1 and 6.")
+                System.out.println("Invalid choice. Please select an option between 1 and 6.");
             }
 
             System.out.println();
         }
-
     }
 
-    public static void showMenu() {
+
+    public void showMenu() {
         System.out.println("Welcome to Student Grade Management System");
         System.out.println("1. Add Student and Grade");
         System.out.println("2. Display All Students and Grades");
@@ -48,23 +54,44 @@ public class StudentGradeManagementSystem {
         System.out.println("6. Exit the Program");
     }
 
-    public static void addStudent() {
+    public void addStudent() {
+        if (count >= 50) {
+            System.out.println("Student list is at full capacity.");
+            return;
+        }
+
+        System.out.print("Enter student name: ");
+        String name = input.nextLine();
+
+        System.out.print("Enter student grade: ");
+        int grade = input.nextInt();
+        input.nextLine();
+
+        if (grade < 0 || grade > 100) {
+            System.out.println("Invalid input! Grade must be between 0 and 100.");
+            return;
+        }
+
+        names[count] = name;
+        grades[count] = grade;
+        count++;
+        System.out.println("Student successfully added.");
 
     }
 
-    public static void displayStudents() {
+    public void displayStudents() {
 
     }
 
-    public static void findStudent() {
+    public void findStudent() {
 
     }
 
-    public static void calculateAverage() {
+    public void calculateAverage() {
 
     }
 
-    public static void findHighestAndLowest() {
+    public void findHighestAndLowest() {
 
     }
 }
